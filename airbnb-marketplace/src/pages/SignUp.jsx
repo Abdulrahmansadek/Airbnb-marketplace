6fe74feb7b8a0f4f27f8d0961e63fef55c864e42
Abdulrahmansadek <boody.sad@gmail.com>
2022-02-25 19:39:38 +0100

    create sign in functunality and sign out

--- airbnb-marketplace/src/pages/SignUp.jsx
+++ airbnb-marketplace/src/pages/SignUp.jsx
@@ -1,7 +1,141 @@
-import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import visibleIcon from "../assets/svg/visibilityIcon.svg";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../firebase.config";
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
 
 function SignUp() {
-  return <div>Sign-up</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const { name, email, password } = formData;
+  const navigate = useNavigate();
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+      const userData = { ...formData };
+      userData.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, "users", user.uid), userData);
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Create new account
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" onSubmit={handleSignUp}>
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <input
+                  id="name"
+                  type="text"
+                  required
+                  value={name}
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="name"
+                  onChange={onChange}
+                />
+              </div>
+              <div>
+                <input
+                  id="email"
+                  type="email"
+                  required
+                  value={email}
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email"
+                  onChange={onChange}
+                />
+              </div>
+
+              <div className="password-container">
+                <input
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  required
+                  value={password}
+                  className="appearance-none rounded-none  block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                  onChange={onChange}
+                />
+                <img
+                  src={visibleIcon}
+                  alt="show password"
+                  className="showPassword"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="text-sm">
+                <Link
+                  to={"/sign-in"}
+                  className="font-medium text-black-600 hover:text-indigo-500"
+                >
+                  Already have account?
+                </Link>
+              </div>
+
+              <div className="text-sm">
+                <Link
+                  to={"/forget-password"}
+                  className="font-medium text-black-600 hover:text-indigo-500"
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-error hover:bg-error-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default SignUp;
